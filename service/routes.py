9709a3e0c8a30cf777caddd8c21bc44357829355
Amritha <sekharamritha92@gmail.com>
2023-07-21 08:57:25 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -147,7 +147,7 @@ def check_content_type(media_type):
     )
 
 def test_method_not_allowed(self):
-        """It should not allow an illegal method call"""
-        resp = self.client.delete(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    """It should not allow an illegal method call"""
+    resp = self.client.delete(BASE_URL)
+    self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
