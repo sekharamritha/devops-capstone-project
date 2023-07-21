4a91ea3938ee1d03344fdbd741b3a6c44a8368b5
Amritha <sekharamritha92@gmail.com>
2023-07-21 10:59:42 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -145,9 +145,3 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
-
-def test_method_not_allowed(self):
-    """It should not allow an illegal method call"""
-    resp = self.client.delete(BASE_URL)
-    self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
